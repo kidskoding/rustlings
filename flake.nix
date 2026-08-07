aaea0adfe1e1f5a29c73d781b491c1efbcb63681
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-08-06 23:37:15 -0500

    added nix dev shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bacon
+          cargo-chef
+          evcxr
+          rustlings
+        ];
+
+        RUST_BACKTRACE = "1";
+      };
+    };
+}
