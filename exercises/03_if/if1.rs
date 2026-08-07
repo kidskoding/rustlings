479af44630df0e4716a592d9c0bb1fedf67c9f4f
Anirudh Konidala <anirudhkonidala@gmail.com>
2026-08-07 02:04:59 -0500

    exercises 1-6

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,13 +1,19 @@
 fn bigger(a: i32, b: i32) -> i32 {
-    // TODO: Complete this function to return the bigger number!
-    // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
+    if a > b {
+        return a;
+    }
+
+    b
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let a = 42;
+    let b = 42;
+    let c = 38;
+
+    println!("the bigger number between a={} and b={} is {}", a, b, bigger(a, b));
+    println!("the bigger number between b={} and c={} is {}", b, c, bigger(b, c));
+    println!("the bigger number between a={} and c={} is {}", a, c, bigger(a, c));
 }
 
 // Don't mind this for now :)
--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -6,9 +6,7 @@ fn main() {
         println!("Good morning!");
     }
 
-    // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
-    // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    let is_evening = true;
     if is_evening {
         println!("Good evening!");
     }
